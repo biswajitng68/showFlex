c7cf69d11aa5655164ce8bb57ce995a6804d08bd
Biswajit Nag <biswajitnag68@gmail.com>
2023-07-26 17:51:31 +0530

    bn

--- components/App.js
+++ components/App.js
@@ -1,12 +1,40 @@
 import { View, Text } from 'react-native'
 import React from 'react'
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons'
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import Home from './Home';
+import Movies from './Movies';
+import Tv from './Tv';
+import { NavigationContainer } from '@react-navigation/native';
+import Icon from 'react-native-vector-icons/FontAwesome'; 
 
-const App = () => {
+const Tab = createMaterialBottomTabNavigator();
+
+function App() {
   return (
-    <View>
-      <Text>App</Text>
-    </View>
-  )
+    <NavigationContainer>
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={Home} options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({ color }) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}/>
+      <Tab.Screen name="Movies" component={Movies} options={{
+          
+          tabBarIcon: ({ color }) => (
+            <Icon name="film" color={color} size={26} />
+          ),
+        }}/>
+      <Tab.Screen name="Tv" component={Tv} options={{
+          tabBarLabel: 'Tv',
+          tabBarIcon: ({ color }) => (
+            <Icon name="tv" color={color} size={26} />
+          ),
+        }}/>
+    </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default App
