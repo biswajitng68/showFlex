b9cd2153a73e645ae397c757a1aaa7f4bcc64fc7
Biswajit Nag <biswajitnag68@gmail.com>
2023-07-31 16:12:38 +0530

    bnn

--- components/Detail.js
+++ components/Detail.js
@@ -1,5 +1,6 @@
 import { Image, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
 import React,{useEffect,useState} from 'react'
+import { blue300 } from 'react-native-paper/lib/typescript/src/styles/themes/v2/colors';
 
 export default function Detail() {
     const base="https://image.tmdb.org/t/p/original"
@@ -22,17 +23,37 @@ export default function Detail() {
   return (
     <SafeAreaView style={{backgroundColor:"black",height:"100%"}}>
         <ScrollView>
-            {movie&&
+            {movie&&<>
             <View style={styles.container}>
                 <Image source={{uri:base+movie.backdrop_path}} style={styles.mainimage}/>
-            </View>}
+            </View>
             <View style={{alignItems:"center"}}>
             <View style={styles.overlay}>
                 <Text style={styles.title}>{movie.title}</Text>
+                <View style={{flexDirection:"row"}}>
+                <Text style={styles.gentext}>{movie.status}</Text>
                 <Text style={styles.gentext}>{movie.runtime} min</Text>
+                </View>
+                
+                <View style={{flexDirection:"row"}}>
+                {movie.genres&&
+                  (()=>{
+                    var genre=[]
+                    for(let i=0;i<movie.genres.length;i++){
+                      genre.push(
+                        
+                        <Text style={styles.gentext} key={i}>{movie.genres[i].name}</Text>
+                      
+                      )
+                    }
+                    return genre;
+                  })()
+                }
+                </View>
                 <Text style={styles.gentext}>Overview: {movie.overview}</Text>
             </View>
-            </View>
+            </View></>}
+            <View style={{width:100,height:100,backgroundColor:"white"}}></View>
         </ScrollView>
     </SafeAreaView>
   )
@@ -52,21 +73,25 @@ const styles = StyleSheet.create({
     bottom: 30,
     left:0,
     right:0,
-    backgroundColor:"#1d4d5e",
+    backgroundColor:"#b5d7f5",
     borderRadius:5,
     width:"90%",
     padding:5,
+    elevation:30,
+    shadowColor:"white"
      // Adjust this value to control the height of the transparent area
    // Change the last value (0.5) to adjust the opacity
   },
   title:{
-    color:"white",
+    color:"black",
     fontSize:20,
     fontWeight:"600",
     
   },
   gentext:{
+    color:"black",
     fontSize:15,
-    fontWeight:"500"
+    fontWeight:"500",
+    padding:5
   }
 })
